1c1523cda131ebe6043ae0ad0a25a193ea4d645c
Sylverb <sylver.bruneau@gmail.com>
2025-01-29 14:17:06 +0100

    Removed useless files

--- Core/Inc/tar.h
+++
@@ -1,9 +0,0 @@
-#ifndef TAR_H
-#define TAR_H
-#include <stdbool.h>
-
-typedef void (*progress_callback_t)(unsigned int percentage, const char *file_name);
-
-bool extract_tar(const char *tar_path, const char *extract_path, progress_callback_t progress_callback);
-
-#endif // TAR_H
